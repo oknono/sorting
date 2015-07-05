de4c18c77081c3d437ee2ef2b0b2aa4777f3b72d
Kirsten Korevaar <kirsten@eml.cc>
2015-07-05 11:04:36 -0400

    change var names and print steps

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,22 +1,20 @@
-# go trough list list left to right
-# If element right is smaller then current element
-# swap element and proceed to next pair
-# keep running through the list until all elements are sorted
+# Swap element and proceed to next pair
+# Keep running through the list until all elements are sorted
 
-def bubblesort(some_list):
+def bubblesort(S):
     is_sorted = False
     while not is_sorted:
         is_sorted = True	
-        for index in range(len(some_list)-1):
-            current = some_list[index]
-            next = some_list[index + 1]
+        for index in range(len(S)-1):
+            print S
+            current = S[index]
+            next = S[index + 1]
             if current > next:
                 is_sorted = False
-                some_list[index] = next
-                some_list[index + 1] = current
-            print some_list
-    return some_list
+                S[index] = next
+                S[index + 1] = current
+    return S
 
 if __name__ == "__main__":
     test_list = [10, 3, 4, 2, 81, 23, 0, 15, 100, 99]
-    print "Bubblesort of [10, 3, 4, 2, 81, 23, 0, 15, 100, 99] is {0}".format(bubblesort(test_list))
+    bubblesort(test_list)
