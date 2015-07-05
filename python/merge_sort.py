5112171937cf2dd1a333cbe7f021aee63a92d211
Kirsten Korevaar <kirsten@eml.cc>
2015-07-05 11:11:02 -0400

    print steps

--- python/merge_sort.py
+++ python/merge_sort.py
@@ -11,21 +11,22 @@ def merge(S1, S2, S):
         else:
             S[i+j]= S2[j]
             j += 1
-    print S
+    print "S is {0}".format(S)
     return S
 
-def merge_sort(some_list):
-    if len(some_list) < 2 :
-        return some_list
+def merge_sort(S):
+    length = len(S)
+    half = int(math.floor(length)) / 2
+    if length < 2 :
+        return S
     else:
-        s1 = some_list[ :int(math.floor(len(some_list))) / 2 ]
-        s2 = some_list[int(math.floor(len(some_list))) / 2 : ]
-        #print s1
-        #print s2
-        merge_sort(s1)
-        merge_sort(s2) 
-        return merge(s1, s2, some_list)
+        S1 = S[ :half]
+        S2 = S[half: ]
+	print "S1 is {0}, S2 is {1}".format(S1, S2)
+        merge_sort(S1)
+        merge_sort(S2) 
+        return merge(S1, S2, S)
 
 if __name__ == "__main__":
     test_list = [10, 3, 4, 2, 81, 23, 0, 15, 100, 99]
-    print "selection sort of [10, 3, 4, 2, 81, 23, 0, 15, 100, 99] is {0}".format(merge_sort(test_list))
+    merge_sort(test_list)
